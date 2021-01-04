90b5268fb3fafdc075e7f12a5c6c13f659bfcefd
James Chen <jamesczq@gmail.com>
2021-01-04 17:52:34 -0500

    Fully-complied Hello World

---
+++ 01_hello/hello.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Author: James
+Purpose: Say hello with command-line arguments
+"""
+
+import argparse
+
+
+def get_args():
+    """Get command-line inputs"""
+    parser = argparse.ArgumentParser(description='Say hello')
+    # Positional arg (mandatory):
+    # parser.add_argument('name', help='Name to greet')
+    parser.add_argument(
+        '-n',
+        '--name',
+        metavar='name',
+        default='World',
+        help='Name to greet')
+    return parser.parse_args()
+
+
+def main():
+    """Main action"""
+    args = get_args()
+    print(f'Hello, {args.name}!')
+
+
+if __name__ == '__main__':
+    main()
